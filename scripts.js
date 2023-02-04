5093b06b998cfd1083406709551521b4f4f421c2
danvitmac <daniel.atdr@gmail.com>
2023-02-04 23:20:43 +0000

    insert api async

--- scripts.js
+++ scripts.js
@@ -1,15 +1,19 @@
 const button = document.getElementById("convert-button")
 const select = document.getElementById("currency-select")
 
-const dolar = 5.2
-const euro = 5.9
-const bitCoin = 109470.1
 
-const convertValues = () => {
+
+const convertValues = async () => {
   const inputReais = document.getElementById("input-real").value
   const realValueText = document.getElementById("real-value-text")
   const currencyValueText = document.getElementById("currency-value-text")
 
+ const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then( dados => dados.json())
+ console.log(data)
+ const dolar = data.USDBRL.high
+ const euro = data.EURBRL.high
+ const bitCoin = data.BTCBRL.high
+  
   realValueText.innerHTML = new Intl.NumberFormat("pt-BR",
     { style: 'currency', currency: "BRL" }
   ).format(inputReais)
